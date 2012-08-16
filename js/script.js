a7ee44e732d73aa60c7f82ace2589533a63ede2b
bargenson <bargenson@gmail.com>
2012-08-16 21:03:06 +0200

    Add certifications & skills

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 var APP = {
 
+	initContent: function() {
+		var hash, content;
+		hash = window.location.hash;
+		content = hash ? hash.substr(1) : "menu";
+		this.changeContent(content);
+	},
+
 	changeContent: function(content) {
 		$.mobile.showPageLoadingMsg();
-		
-		(function displayOrHideBackButton(content) {
-			var header = $("#page-header");
-			if(content != "menu") {
-				console.log("!menu");
-				if(!header.children(".back-button").length) {
-					console.log("!children");
-					header.prepend("<a class='back-button' href='#menu' data-icon='back'>Back</a>");
-				}
-			} else {
-				if(header.children(".back-button").length) {
-					console.log("children");
-					header.children(".back-button").remove();
-				}
-			}
-		})(content);
-
 		$.get('pages/' + content + '.mustache.html', function(page) {
+			(function displayOrHideBackButton(content) {
+				var header = $("#page-header");
+				if(content != "menu") {
+					if(!header.children(".back-button").length) {
+						header.prepend("<a class='back-button' href='#menu' data-icon='back'>Back</a>");
+					}
+				} else {
+					if(header.children(".back-button").length) {
+						header.children(".back-button").remove();
+					}
+				}
+			})(content);
 			$("div[role='main']").html(page);
 			$("#page").page('destroy').page();
 			$.mobile.hidePageLoadingMsg();
@@ -28,10 +30,8 @@ var APP = {
 };
 
 $(document).ready( function(event) {
-	var hash, content;
-	hash = window.location.hash;
-	content = hash ? hash.substr(1) : "menu";
-	APP.changeContent(content);
+	
+	APP.initContent();
 	
 	$(document).on("click", "a", function(event) {
 		var hash = $(this).attr("href");
@@ -39,5 +39,9 @@ $(document).ready( function(event) {
 			window.location.hash = hash;
 			APP.changeContent(hash.substr(1));
 		}
+	});
+
+	$(window).hashchange(function() {
+		APP.initContent();
 	});
 });
